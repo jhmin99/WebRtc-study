52387bc9141fe6bd6fee71fdaf1066cf39ad57c6
jhmin99 <mjh012399@gmail.com>
2024-09-11 11:29:38 +0900

    Init front

--- src/server.js
+++ src/server.js
@@ -3,6 +3,11 @@ import express from "express"
 
 const app = express();
 
-console.log("hello");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
 
-app.listen(3000);
+app.get("/", (req, res) => res.render("home"));
+const handleListen = () => console.log("Listening on http://127.0.0.1:3000/");
+
+app.listen(3000, handleListen)
